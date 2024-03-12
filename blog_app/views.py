6400702a033a8587ab59282e95d7204f82648995
Sainaveen Bhukya <bhukya.sainaveen@gmail.com>
2024-03-12 19:12:08 +0530

    Added basic frontend for blogging website

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog_app.models import Post
 
 # Create your views here.
+def get_all_posts(request):
+    posts = Post.objects
+    return render(request, 'posts.html', {'posts': posts})
+
+def details(request, blog_id=None):
+    details = get_object_or_404(Post, pk = blog_id)
+    return render(request, 'details.html', {'post':details})
+
+
