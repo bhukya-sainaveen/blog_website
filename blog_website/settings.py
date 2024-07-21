68a1b2df8251c15f0158bdda5e4c795dca605609
Bhukya Sainaveen <bhukya.sainaveen@gmail.com>
2024-07-21 18:00:16 +0530

    Update settings.py

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j-v6)yg(q&%lvdfrp-$q20718_lc9um@ujoeq01703psxr7i9j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-website-5-343x.onrender.com]
 
 
 # Application definition
